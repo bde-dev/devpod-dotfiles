8fd3850348f13f9b11390acc7adccf103810ad10
Brad <brad.evans0925@gmail.com>
2025-05-14 10:23:36 +0100

    just keep all lsp and extras clean on newdevpod

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.python" },
     -- { import = "lazyvim.plugins.extras.lang.terraform" },
     -- { import = "lazyvim.plugins.extras.lang.toml" },
-    { import = "lazyvim.plugins.extras.lang.yaml" },
+    -- { import = "lazyvim.plugins.extras.lang.yaml" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
